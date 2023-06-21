531e33213d6e199367e6feffac1bde2c59ea5cde
Emilio <shurfrock.kami@gmail.com>
2023-06-21 14:29:59 -0600

    Proyecto Agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'products/:id',
+    component: ProductsComponent
+  },
+  {
+    path:'newProduct',
+    component: NewProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
